a96f5572a87a42aa3021d3dc7b81f512b6e4180b
Manish <deadmanish7@gmail.com>
2023-04-20 21:02:39 +0530

    changed some http in script to https

--- assets/script/home.js
+++ assets/script/home.js
@@ -1,5 +1,5 @@
 // const server="https://rebazaar.onrender.com";
-const server="http://3.140.94.217";
+const server="https://3.140.94.217";
 
 function getCookie(cname) {
     let name = cname + "=";
