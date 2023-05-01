13f34ed72b3ef8a511d30122324375c83e2dc7e9
Manish <deadmanish7@gmail.com>
2023-05-01 07:09:49 +0530

    random

--- serverNode.js
+++ serverNode.js
@@ -16,7 +16,7 @@ const {returnId} = require('./backend/protectRoute.js');
 const {sellRequestMail, adminVerifyMail} = require('./backend/nodemailer');
 const path = require('path');
 const fs = require('fs');
-const PORT = process.env.PORT
+const PORT = 3000 || process.env.PORT
 
 app.use(express.json());
 // app.use(express.urlencoded());
